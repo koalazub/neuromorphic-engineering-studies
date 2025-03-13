000db1d79828672e50d7d8a78c40f44f77eadc51
koalazub <7111524+koalazub@users.noreply.github.com>
2025-03-13 21:41:50 +1100

    feat: complete calculations
    also cleaned up directory for structures' sake. May revert to monolithic style later

---
+++ data_science/week1/calculations.R
@@ -0,0 +1,13 @@
+result_a <- 23 * 43
+
+result_b <- 42^3
+
+result_c <- (34 / 45) * sqrt(2)
+
+result_d <- 2^2^2
+
+result_e <- log(10)
+
+result_f <- 1 / sqrt(2 * pi) * exp(-1.4)
+
+result_g <- (5 * pi) / (2 * exp(-3))
