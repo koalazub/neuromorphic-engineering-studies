0882ad02b4d536c5392810799d0e8476d5019cdd
koalazub <7111524+koalazub@users.noreply.github.com>
2025-03-13 21:09:56 +1100

    nit: linter complaints

--- data_science/analyse.R
+++ data_science/analyse.R
@@ -1,15 +1,10 @@
 library(ggplot2)
 
-# Define constant for dataset URL
-DATA_URL <- "http://staff.scm.uws.edu.au/~lapark/download/data/student-mat.csv"
+data_url <- "http://staff.scm.uws.edu.au/~lapark/download/data/student-mat.csv"
 
-# Rename data variable to something descriptive
-student_data <- read.csv(DATA_URL, sep = ";")
-
-# Extract the linear model into its own variable with a clear name
+student_data <- read.csv(data_url, sep = ";")
 grade_vs_age_model <- lm(G3 ~ age, data = student_data)
 
-# Plot data with clear labels
 plot(
   student_data$age,
   student_data$G3,
@@ -17,8 +12,6 @@ plot(
   ylab = "Final Grade (G3)"
 )
 
-# Add regression line
 abline(grade_vs_age_model)
 
-# Display model summary
 summary(grade_vs_age_model)
