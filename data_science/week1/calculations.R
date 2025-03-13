61d52efec1bee7d4c5bcde3daea7098cdfa52e53
koalazub <7111524+koalazub@users.noreply.github.com>
2025-03-13 21:56:15 +1100

    chore: pretty up the output of the calculations

--- data_science/week1/calculations.R
+++ data_science/week1/calculations.R
@@ -11,3 +11,11 @@ result_e <- log(10)
 result_f <- 1 / sqrt(2 * pi) * exp(-1.4)
 
 result_g <- (5 * pi) / (2 * exp(-3))
+
+cat("a) 23 * 43 = ", result_a, "\n")
+cat("b) 42^3 = ", result_b, "\n")
+cat("c) (34 / 45) * sqrt(2) = ", result_c, "\n")
+cat("d) 2^2^2 = ", result_d, "\n")
+cat("e) log(10) = ", result_e, "\n")
+cat("f) 1 / sqrt(2 * pi) * exp(-1.4) = ", result_f, "\n")
+cat("g) (5 * pi) / (2 * exp(-3)) = ", result_g, "\n")
