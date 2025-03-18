05746afb3685528f94d75c4905e9446db32e48ca
koalazub <7111524+koalazub@users.noreply.github.com>
2025-03-18 13:00:38 +1100

    chore: jupyter setup with flake

--- signal_processing/flake.nix
+++ signal_processing/flake.nix
@@ -1,42 +1,25 @@
 {
-  description = "Development environment with Python, Mojo, Jupyter, and Julia";
+  description = "Development environment with Mojo, Jupyter, and Julia";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config = {
             allowUnfree = true; # Mojo currently has a proprietary license
           };
-        };
-
-        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-          jupyter
-          notebook
-          ipykernel
-          numpy
-          pandas
-          matplotlib
-        ]);
-
-        juliaEnv = pkgs.mkShell {
-          buildInputs = [ pkgs.julia-bin ]; # Use the binary distribution
-          shellHook = ''
-            julia -e '
-              using Pkg
-              try
-                Pkg.add("LanguageServer")
-              catch e
-                @warn "Failed to install LanguageServer: $e"
-              end
-            '
-          '';
         };
 
         # Placeholder for Mojo - you'll need to figure out how to install it
@@ -47,10 +30,8 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pythonEnv
-            juliaEnv
             #mojo # Include when you have a valid Mojo package
-            pkgs.nushell # Adding nushell for the shellHook
+            pkgs.jupyter
           ];
 
           shellHook = ''
@@ -62,9 +43,6 @@
           '';
         };
 
-        packages = {
-          pythonEnv = pythonEnv;
-        };
       }
     );
 }
