1fd645f0759c3d6ad21d0dd20c2d9aeab03c77c2
koalazub <7111524+koalazub@users.noreply.github.com>
2025-03-26 22:06:10 +1100

    feat: setup questions and files for project
    Also updated flake to include julia configurations

--- signal_processing/flake.nix
+++ signal_processing/flake.nix
@@ -1,16 +1,18 @@
 {
-  description = "Development environment with Mojo, Jupyter, and Julia";
+  description = "Mathematics of Signal Processing dev env";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    mojoPlayground.url = "github:franckrasolo/mojo-playground.nix";
   };
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      mojoPlayground,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -18,31 +20,33 @@
         pkgs = import nixpkgs {
           inherit system;
           config = {
-            allowUnfree = true; # Mojo currently has a proprietary license
+            allowUnfree = true;
           };
         };
 
-        # Placeholder for Mojo - you'll need to figure out how to install it
-        #mojo = null; # Replace with a valid Mojo package if you can find one
-        #mojo = pkgs.callPackage ./path/to/mojo.nix { }; # Example using a local definition
-
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            #mojo # Include when you have a valid Mojo package
-            pkgs.jupyter
-          ];
 
+        devShells.default = pkgs.mkShell {
+          name = "maths-signal-processing";
+          buildInputs = with pkgs; [
+            jupyter
+            git
+            uv
+          ];
           shellHook = ''
-            if command -v nu >/dev/null 2>&1; then
+            if command -v nu >/dev/null; then
               exec nu
             else
-              echo "nu command not found, skipping shellHook"
+              echo "nu not found, skipping shell hook"
             fi
           '';
         };
 
+        devShells.mojo = mojoPlayground.lib.mkShell {
+          inherit system;
+          projectName = "maths-signal-processing";
+        };
       }
     );
 }
