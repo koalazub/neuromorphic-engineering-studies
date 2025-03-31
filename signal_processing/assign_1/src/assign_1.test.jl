9ed0a41fa87dcd9214f4c75f547a141cda0cdc57
koalazub <7111524+koalazub@users.noreply.github.com>
2025-04-01 00:06:46 +1100

    feat: solved for q1 and q2
    Q3 will need to be done later. Need to learn Julia and get the stupid LSP running correctly

--- signal_processing/assign_1/src/assign_1.test.jl
+++
@@ -1,8 +0,0 @@
-using Test
-using assign_1
-
-@testset "Basic function test" begin
-    @test add(1, 3) == 3
-
-    @test_throws ArgumentError add(-1)
-end
--- signal_processing/assign_1/src/script.jl
+++
@@ -1,8 +0,0 @@
-# Edit your script.jl to use:
-include("assign_1.jl")
-using .Assign1  # Note the dot before Assign1
-
-Assign1.run_demo()
-
-result = Assign1.add_numbers(5, 7)
-plot = Assign1.create_interactive_linear_graph()
